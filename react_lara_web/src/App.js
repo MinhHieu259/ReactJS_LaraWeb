d1322910e6a1f291a9f3b848ec8d73451b0db7c5
MinhHieu259 <mhieu7252@gmail.com>
2022-01-17 17:51:59 +0700

    first commit

---
+++ react_lara_web/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Masterlayout from './layouts/admin/Masterlayout';
+import Home from './components/frontend/Home';
+import Login from './components/frontend/auth/Login';
+import Register from './components/frontend/auth/Register';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+              <Route path="/admin" name="Admin" render={(props) => <Masterlayout {...props}/>}/>
+          </Switch>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_lara_web/src/layouts/frontend/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Navbar() {
+    return (
+       <div>
+           <h1>Register Page</h1>
+           <button className='btn btn-primary'>bhs</button>
+       </div>
+    );
+}
+export default Navbar;
