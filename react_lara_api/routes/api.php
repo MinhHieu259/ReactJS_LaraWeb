d28f89e7515341e964084f3429b611d590f905ea
MinhHieu259 <mhieu7252@gmail.com>
2022-01-27 23:20:26 +0700

    react

--- react_lara_api/routes/api.php
+++ react_lara_api/routes/api.php
@@ -8,9 +8,13 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/checkingAuthenticated', function () {
+        return response()->json(['message' => 'Đã đăng nhập', 'status' => 200], 200);
+    });
+
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
--- react_lara_web/src/AdminPrivateRoute.js
+++ react_lara_web/src/AdminPrivateRoute.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import swal from 'sweetalert';
+import Masterlayout from './layouts/admin/Masterlayout';
+
 
 
 function AdminPrivateRoute(...rest) {
-    // return (
-    //     <Route {...rest}
-    //         render={({props, location}) => 
-    //             localStorage.getItem('auth_token');
+    const history = useHistory();
+    const [Authenticated, setAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        axios.get(`api/checkingAuthenticated`).then(res => {
+            if (res.data.status === 200) {
+                setAuthenticated(true);
+            }
+            setLoading(false);
+        });
+        return () => {
+            setAuthenticated(false);
+        };
+    }, []);
 
-    //         }
-    //      />
-    // );
+    axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
+        if (err.response.status === 401) {
+            swal('Lỗi', "Chưa đăng nhập Admin", "warning");
+            history.push('/');
+
+        }
+        return Promise.reject(err);
+    });
+
+    if (loading) {
+        return <h1>Đang tải...</h1>
+    }
+    return (
+        <Route {...rest}
+            render={({ props, location }) =>
+                Authenticated ?
+                    (<Masterlayout {...props} />) :
+                    (<Redirect to={{ pathname: "/login", state: { from: location } }} />)
+
+            }
+        />
+    );
 }
 
 export default AdminPrivateRoute;
