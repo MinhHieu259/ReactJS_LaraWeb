a17e6470788ad224e456f763a53ff83ab4cc6501
MinhHieu259 <mhieu7252@gmail.com>
2022-03-05 18:02:50 +0700

    add detail pro

--- react_lara_api/app/Http/Controllers/API/FrontendController.php
+++ react_lara_api/app/Http/Controllers/API/FrontendController.php
@@ -44,4 +44,28 @@ class FrontendController extends Controller
             ]);
         }
     }
+
+    public function viewproduct($category_slug, $product_slug)
+    {
+        $category = Category::where('slug', $category_slug)->where('status', '0')->first();
+        if($category){
+            $product = Product::where('category_id', $category->id)->where('slug', $product_slug)->where('status', '0')->first();
+            if($product){
+                return response()->json([
+                    'status' => 200,
+                    'product' => $product
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 400,
+                    'message' => 'Sản phẩm không tồn tại'
+                ]);
+            }
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Danh mục không tồn tại'
+            ]);
+        }
+    }
 }
